1a4821bf864fbefee2bccb640a2f91d10047afd5
Daniel Strommen <nerdile@outlook.com>
2020-05-25 13:03:48 -0700

    Initial commit

---
+++ PasswordGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace NerdileSnippets
+{
+    public static class PasswordGenerator
+    {
+        public static string GeneratePassword(int length)
+        {
+            var random = new RNGCryptoServiceProvider();
+            var data = new byte[length];
+            random.GetNonZeroBytes(data);
+            return Convert.ToBase64String(data).Substring(0, length);
+        }
+    }
+}
